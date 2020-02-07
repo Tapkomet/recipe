770cbf09e22eb984815247f7008c548f803e7fe7
Tapkomet <romakobzar@ukr.net>
2020-02-07 02:07:05 +0200

    Added Argument Capture to the test with mocks for IndexController

--- src/test/java/tapkomet/springframework/recipe/controllers/IndexControllerTest.java
+++ src/test/java/tapkomet/springframework/recipe/controllers/IndexControllerTest.java
@@ -2,6 +2,7 @@ package tapkomet.springframework.recipe.controllers;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.ui.Model;
@@ -18,13 +19,17 @@ import static org.mockito.Mockito.*;
 class IndexControllerTest {
 
 
+    private static final Recipe TEST_RECIPE = new Recipe();
+    private static final Recipe[] RECIPES_ARRAY = new Recipe[] {TEST_RECIPE, new Recipe()};
+    private static final Set<Recipe> TEST_RECIPES = new HashSet<>(Arrays.asList(RECIPES_ARRAY));
+
     @Mock
     RecipeService recipeService;
 
     @Mock
     Model model;
 
-    private IndexController indexController;
+    IndexController indexController;
 
     @BeforeEach
     void setUp() {
@@ -34,8 +39,13 @@ class IndexControllerTest {
 
     @Test
     void getIndexPage() {
+        when(recipeService.getRecipes()).thenReturn(TEST_RECIPES);
+        ArgumentCaptor<Set<Recipe>> argumentCaptor = ArgumentCaptor.forClass(Set.class);
+
+        //when(model.addAttribute("recipes", recipeService.getRecipes())).thenReturn(model);
         assertEquals("index", indexController.getIndexPage(model));
         verify(recipeService, times(1)).getRecipes();
-        verify(model, times(1)).addAttribute(eq("recipes"), anySet());
+        verify(model, times(1)).addAttribute(eq("recipes"), argumentCaptor.capture());
+        assertEquals(TEST_RECIPES, argumentCaptor.getValue());
     }
 }
