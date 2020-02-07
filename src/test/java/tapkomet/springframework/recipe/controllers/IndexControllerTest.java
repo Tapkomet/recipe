135168b4c733541956d70f8783e2623a34a682d9
Tapkomet <romakobzar@ukr.net>
2020-02-07 02:24:39 +0200

    Added Spring MVC and Integration tests examples

--- src/test/java/tapkomet/springframework/recipe/controllers/IndexControllerTest.java
+++ src/test/java/tapkomet/springframework/recipe/controllers/IndexControllerTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.ui.Model;
 import tapkomet.springframework.recipe.domain.Recipe;
 import tapkomet.springframework.recipe.services.RecipeService;
@@ -15,12 +17,14 @@ import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 class IndexControllerTest {
 
-
     private static final Recipe TEST_RECIPE = new Recipe();
-    private static final Recipe[] RECIPES_ARRAY = new Recipe[] {TEST_RECIPE, new Recipe()};
+    private static final Recipe[] RECIPES_ARRAY = new Recipe[]{TEST_RECIPE, new Recipe()};
     private static final Set<Recipe> TEST_RECIPES = new HashSet<>(Arrays.asList(RECIPES_ARRAY));
 
     @Mock
@@ -35,6 +39,15 @@ class IndexControllerTest {
     void setUp() {
         MockitoAnnotations.initMocks(this);
         indexController = new IndexController(recipeService);
+    }
+
+    @Test
+    void testMockMVC() throws Exception {
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(indexController).build();
+
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("index"));
     }
 
     @Test
---
+++ src/test/java/tapkomet/springframework/recipe/repositories/UnitOfMeasureRepositoryIT.java
@@ -0,0 +1,35 @@
+package tapkomet.springframework.recipe.repositories;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit4.SpringRunner;
+import tapkomet.springframework.recipe.domain.UnitOfMeasure;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(SpringExtension.class)
+@DataJpaTest
+class UnitOfMeasureRepositoryIT {
+
+    @Autowired
+    UnitOfMeasureRepository unitOfMeasureRepository;
+
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    @Test
+    void findByDescription(){
+
+        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByName("Teaspoon");
+
+        assertEquals("Teaspoon", unitOfMeasureOptional.get().getName());
+    }
+}
